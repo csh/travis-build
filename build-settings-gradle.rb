2e15733abaa2c39899be7cfb91c473b3651b9f04
Connor Spencer Harries <connor@smirking.ninja>
2015-08-15 17:27:43 +0100

    Create build-settings-gradle.rb

---
+++ build-settings-gradle.rb
@@ -0,0 +1,14 @@
+if ENV['TRAVIS_SECURE_ENV_VARS'] != 'true'
+  puts 'Repository password not found, skipping deployment'
+  exit
+end
+
+repository_username = ENV['REPOSITORY_USERNAME'] || 'deployment'
+repository_password = ENV['REPOSITORY_PASSWORD']
+
+properties = "nexus_username=#{repository_username}\nnexus_password=#{repository_password}"
+path = File.join(File.expand_path('~'), '.gradle', 'gradle.properties')
+File.open(path, 'w') { |file|
+  puts 'Writing gradle properties file'
+  file.write properties
+}
