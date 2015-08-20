a52fb2f86ecd3483cde1ad84edd3e937ce75c9bd
Connor Spencer Harries <connor@smirking.ninja>
2015-08-20 21:55:51 +0100

    Update build-settings-gradle.rb

--- build-settings-gradle.rb
+++ build-settings-gradle.rb
@@ -4,8 +4,8 @@ if ENV['TRAVIS_SECURE_ENV_VARS'] != 'true'
 end
 
 repository_username = ENV['REPOSITORY_USERNAME'] || 'deployment'
-repository_password = ENV['REPOSITORY_PASSWORD']
-github_token = ENV['GITHUB_TOKEN']
+repository_password = ENV['REPOSITORY_PASSWORD'] || ''
+github_token = ENV['GITHUB_TOKEN'] || ''
 
 properties = "nexus_username=#{repository_username}\nnexus_password=#{repository_password}\ngithub_token=#{github_token}"
 path = File.join(File.expand_path('~'), '.gradle')
