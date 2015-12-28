77e525c02b70455485f645e94a1e4b3773c699dc
Connor Spencer Harries <connor@smirking.ninja>
2015-12-28 14:35:03 +0000

    Include build-settings.sh

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'nokogiri', '~> 1.6.6.2'
--- build-settings.sh
+++ build-settings.sh
@@ -1,3 +1,22 @@
-VERSION=`mvn -o help:evaluate -Dexpression=project.version | grep -v '\['`
+#if [ "$TRAVIS_SECURE_ENV_VARS" == "" ]; then
+#	echo "Could not find repository credentials"
+#	exit 1
+#fi
 
-echo $VERSION
+TARGET_DIR="$HOME/.m2"
+TARGET_FILE="$TARGET_DIR/settings.xml"
+
+mkdir -p "$TARGET_DIR"
+
+read -r -d '' settings << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<settings>
+  <servers>
+    <id>$REPOSITORY_ID</id>
+    <username>$REPOSITORY_USERNAME</username>
+    <password>$REPOSITORY_PASSWORD</password>
+  </servers>
+</settings>
+EOF
+
+echo "$settings" >> "$TARGET_FILE"
